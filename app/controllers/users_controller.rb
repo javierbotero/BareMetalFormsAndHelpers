5c637a5b0c901536df800d78f96b52e9a190af89
Javier Botero <javierbotero10271@gmail.com>
2020-05-28 13:09:51 -0500

    Add the if to show errors in new and edit views, also the README.md info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,18 @@ class UsersController < ApplicationController
 		render :new
 	  end
 	end
+
 	def edit
 		@user = User.find(params[:id])
+	end
+
+	def update
+	  @user = User.find(params[:id])
+	  if @user.update(user_params)
+		redirect_to edit_user_path
+	  else
+	    render 'edit'
+	  end
 	end
 
 	private
